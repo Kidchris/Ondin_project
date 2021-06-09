07892c793c23fef509a2b0f6c10e2e7dcdc24838
Kidchris <christianjosaphat0@gmail.com>
2021-06-09 09:30:13 +0000

    commited from vscode

---
+++ main.js
@@ -0,0 +1,7 @@
+// console.log("89797" + 987);
+// console.log('on est la et on se demande ce que sera le future');
+// let a = 10;
+// console.log(9 * a);
+// let b = 7 * a;
+// console.log(b);
+// confirm('vous voulez bien me dire sis vous avez un minmun de 78 ans ecrit sur votre carte ddentiter....???');
